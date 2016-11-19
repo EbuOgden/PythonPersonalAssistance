4793357e0f3f576a119edb85f83ec30917626766
Ebubekir <ebubekirogden@gmail.com>
2016-11-19 15:01:24 -0800

    Bash script upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install.sh
+
+# This script helps to install necessary packages
+# Written by Ebubekir Ogden
+
+sudo easy_install pip           #install PiP
+
+sudo pip install wikipedia      #install Wikipedia
+
+sudo pip install wolframalpha   #install Wolframalpha
+
+clear                           #clear
